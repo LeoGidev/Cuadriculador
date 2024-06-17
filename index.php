a32ddc44af227a651c9301b7cbb31bc775a4cf1b
LeoGidev <leogimenez.dev@gmail.com>
2024-06-17 18:27:06 -0300

    se trabaja en html

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <DOCTYPE HTML>
     <html lang="en">
 <head>
@@ -21,10 +22,47 @@
 <body>
     <header>
         <!-- Barra de navegación -->
-        <?php
-include 'recursos/barra.php';       
-?>
-</header>
+        
+   <nav class="navbar navbar-expand-sm navbar-dark fixed-top">
+      <a class="navbar-brand" href="../../index.html">Leo Developer</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Demos
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <h6 class="ml-3"> Proyectos</h6>
+              <a class="dropdown-item" href="https://gored.rf.gd/" target="_blank">Gored web page</a>
+              <a class="dropdown-item" href="http://tecnop.rf.gd/" target="_blank">Tecnop web page</a>
+              <a class="dropdown-item" href="http://yplef.rf.gd/" target="_blank">YPLEF web page</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/Maps/">Maps monitor</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/Reboot/">Device Remote Reboot</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/diagnostic/">Diagnostic</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/Telemeter/">Device Telemeter </a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/CompiApp/">CompiApp(Python)</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/SeleniumApp/">SeleniumApp(Python)</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/velociraptorApp/">Velociraptor App(Python)</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/TecnicAssistant/">Tecnic Assistant(Python)</a>
+              <a class="dropdown-item" href="../../Demo_Proyectos/TagsApp/">TagsApp(Python)</a>
+              <div class="dropdown-divider"></div>
+              <h6 class="ml-3">Prácticas</h6>
+              <a class="dropdown-item" href="../../index.html#practicas" >Ver prácticas <i class="fa-solid fa-arrow-up-right-from-square fa-bounce"></i></a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../index.html#Sobremi">Sobre mí</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../index.html#proyectos">Proyectos</a>
+          </li>
+          
+        </ul>
+      </div>
+    </nav></header>
 <div class='container'>
   <div class='row justify-content-center'>
     <div class="col-12 contenido2">
@@ -37,15 +75,34 @@ include 'recursos/barra.php';
               <label for="colorLineas">Color de las líneas:</label>
               <input class="form-control form-control-color" type="color" id="colorLineas" value="#00ff00" onchange="cambiarColorLineas()">
             </li>
+            <li class='m-3'>
+            <label for="setcant">Cantidad de líneas:</label>
+              <input type="range" id="setcant" min="8" max="11" value="5" onchange="cambiarCantLineas()">
+            <span id="cantLineas">5</span>
+            </li>
+            <li class='m-3'>
+            <label for="setgro">Grosor:</label>
+              <input type="range" id="setgro" min="1" max="20" value="1" onchange="Grosor()">
+            <span id="grosorLineas">1</span>
+            </li>
+            <li class='m-3'>
+            <label for="tra">Transparencia:</label>
+              <input type="range" id="tra" min="0" max="1" value="1" step="0.01" onchange="transparent()">
+            <span id="transpLineas">1</span>
+            </li>
             <li>
               <label for="valor">Imagen:</label>
               <input class="form-control" type="file" id="imagenFondoInput" accept="image/*" onchange="cargarImagenDeFondo()">
             </li>
-            <!-- Botón para descargar la imagen -->
+            
             <li>
               <button class="btn btn-primary m-3" onclick="descargarImagen()">Descargar Imagen</button>
             </li>
+            
+            
+
           </ul>
+          
     </div>
     <div class="container contenido">
        <!-- Contenedor del canvas -->
@@ -57,6 +114,5 @@ include 'recursos/barra.php';
     </div>
 
 </div>
-        <script src="script.js"></script>
+        <script src="script2.js"></script>
 </body>
-  
